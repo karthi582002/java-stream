b7655cde0489b5cb704f0111bb80f3458ed9926f
karthi582002 <karthi582002@gmail.com>
2026-04-15 09:06:10 +0530

    all files added

--- src/test/java/com/karthi/examples/WorkingWithStreams.java
+++ src/test/java/com/karthi/examples/WorkingWithStreams.java
@@ -8,6 +8,6 @@ public class WorkingWithStreams {
 
     @Test
     void steams() {
-        List<String> names = List.of("Amigoscode", "Alex", "Zara");
+        List<String> names = List.of("Karthi", "Alex", "Zara");
     }
 }
