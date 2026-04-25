f11a31de465acf7c0538a7f43499cce69c9d1803
karthi582002 <karthi582002@gmail.com>
2026-04-25 20:24:45 +0530

    Added imperative and declarative approach

--- src/test/java/com/karthi/examples/GettingStarted.java
+++ src/test/java/com/karthi/examples/GettingStarted.java
@@ -5,6 +5,7 @@ import com.karthi.mockdata.MockData;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -15,10 +16,28 @@ public class GettingStarted {
         // 1. Find people aged less or equal 18
         // 2. Then change implementation to find first 10 people
         List<Person> people = MockData.getPeople();
+        List<Person> youngPeople = new ArrayList<>();
+        int limit = 10;
+        int count = 0;
+        for(Person person : people) {
+            if(person.getAge() <= 18) {
+                youngPeople.add(person);
+                count++;
+                if (count == limit) {
+                    break;
+                }
+            }
+        }
+        youngPeople.forEach(System.out::println);
     }
 
     @Test
     public void declarativeApproachUsingStreams() throws Exception {
         List<Person> people = MockData.getPeople();
+        List<Person> youngPeople = people.stream()
+                .filter(p-> p.getAge() <=18)
+                .limit(10)
+                .toList();
+        youngPeople.forEach(System.out::println);
     }
 }
